755d73899a57e2d6b54a64aeef3ebedd6a260bc8
games647 <games647@users.noreply.github.com>
2024-08-19 15:49:40 +0200

    Use string templates

---
+++ src/network.ts
@@ -0,0 +1,41 @@
+const HOST = "http://localhost:8111"
+
+export interface HudEvents {
+    events: [],
+    damage: Damage[]
+};
+
+export interface Damage {
+    // increasing id
+    id: number,
+    // battle log message
+    msg: string,
+    // always empty atm
+    sender: string,
+    // always false atm
+    enemy: boolean,
+    // represents squad, team, all, etc, but is always empty atm
+    mode: string,
+    // battle time in seconds
+    time: number
+};
+
+async function query(url: string): Promise<Response> {
+    const response = await fetch(`${HOST}/${url}`, {
+        method: 'GET',
+        headers: {
+            Accept: 'application/json',
+        },
+    });
+
+    if (!response.ok) {
+        throw new Error(`Unexpected response code: ${response.status}`);
+    }
+
+    return response;
+}
+
+export async function fetchHUD(seenEvent: number, seenDamange: number): Promise<HudEvents> {
+    const events = await (await query(`hudmsg?lastEvt=${seenEvent}&lastDmg=${seenDamange}`)).json();
+    return events as HudEvents;
+}
