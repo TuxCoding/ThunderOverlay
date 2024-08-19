92642ea62da7c352039464c3d43d03e4186e9885
games647 <games647@users.noreply.github.com>
2024-08-19 15:45:21 +0200

    Move team tests to getAvatar

--- tests/team.test.ts
+++ tests/team.test.ts
@@ -1,52 +1,19 @@
-import { DestroyMessage } from "../src/index";
 import { getSquadAvatar, isSquadRelevant } from "../src/team";
 
 describe('Squad member check', () => {
-    const otherPlayerMsg: DestroyMessage = {
-        killer: "-GFF7- somebody",
-        destroyerTank: "▄F-5E",
-        destroyedTank: "ZSU-23-4M",
-        killed: "[CoyC] DRAGON#28"
-    }
     test('Not relevant', () => {
-        expect(isSquadRelevant(otherPlayerMsg)).toBeFalsy();
-    })
-
-    const consolePlayerMsg: DestroyMessage = {
-        killer: "-GFF7- ⋇l-IlIllIIlIIllI",
-        destroyerTank: "▄F-5E",
-        destroyedTank: "ZSU-23-4M",
-        killed: "[CoyC] DRAGON#28"
-    }
-    test('Console player', () => {
-        expect(isSquadRelevant(consolePlayerMsg)).toBeTruthy();
-    })
-
-    const squadKiller: DestroyMessage = {
-        killer: "-GFF7- nudel28",
-        destroyerTank: "▄F-5E",
-        destroyedTank: "ZSU-23-4M",
-        killed: "[CoyC] DRAGON#28"
-    }
-    test('Squad destroyed somebody', () => {
-        expect(isSquadRelevant(squadKiller)).toBeTruthy();
-    })
-
-    const squadKillerDestroyed: DestroyMessage = {
-        killer: "-GFF7- somebody",
-        destroyerTank: "▄F-5E",
-        destroyedTank: "ZSU-23-4M",
-        killed: "[CoyC] nudel28"
-    }
-    test('Squad got destroyed', () => {
-        expect(isSquadRelevant(squadKillerDestroyed)).toBeTruthy();
+        expect(isSquadRelevant("-GFF7- CassualTux (Merkava Mk.1B) zerstört [CoyC] DRAGON#28 (BMP-2)")).toBeTruthy();
     })
 });
 
 describe('find avatar', () => {
-    test('Not existing', () => {
+    test('Not relevant', () => {
         expect(getSquadAvatar("somebody")).toBeNull();
-    });
+    })
+
+    test('Console player', () => {
+        expect(getSquadAvatar("⋇l-IlIllIIlIIllI")).toBe("cardicon_fem_ru_modern_01");
+    })
 
     const KNOWN_SQUAD_MEMBERS = [
         ["CassualTux"],
