2f23ee7e22d1b58037b7479a7fb1061363838246
games647 <games647@users.noreply.github.com>
2024-08-19 15:47:21 +0200

    Refactor network code into new module

--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,6 @@
 import { findVehicleFile } from "./assets";
-import { getSquadAvatar } from "./team";
-
-const HOST = "http://localhost:8111/"
-
-export interface HudEvents {
-    events: [],
-    damage: Damage[]
-};
-
-export interface Damage {
-    // increasing id
-    id: number,
-    // battle log message
-    msg: string,
-    // always empty atm
-    sender: string,
-    // always false atm
-    enemy: boolean,
-    // represents squad, team, all, etc, but is always empty atm
-    mode: string,
-    // battle time in seconds
-    time: number
-};
-
-async function query(url: string): Promise<Response> {
-    const response = await fetch(HOST + url, {
-        method: 'GET',
-        headers: {
-            Accept: 'application/json',
-        },
-    });
-
-    if (!response.ok) {
-        throw new Error(`Unexpected response code: ${response.status}`);
-    }
-
-    return response;
-}
-
-async function fetchHUD(seenEvent: number, seenDamange: number): Promise<HudEvents> {
-    const events = await (await query(`hudmsg?lastEvt=${seenEvent}&lastDmg=${seenDamange}`)).json();
-    return events as HudEvents;
-}
+import { Damage, fetchHUD } from "./network";
+import { getSquadAvatar, isSquadRelevant } from "./team";
 
 async function updateHUD(seenEvent: number, seenDamange: number) {
     try {
