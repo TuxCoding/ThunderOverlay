518d95ec47d7d88cce7ed523360989d3fa2bcc29
games647 <games647@users.noreply.github.com>
2024-08-30 12:57:18 +0200

    Check if file mappings are set to available files

--- tests/team.test.ts
+++ tests/team.test.ts
@@ -1,4 +1,15 @@
 import { getSquadAvatar, isSquadRelevant } from "../src/team";
+import * as fs from 'fs';
+import { AVATAR_FILE_PATH, FILE_EXT } from "../src/assets";
+
+const KNOWN_SQUAD_MEMBERS = [
+    ["TuxCode"],
+    ["Lukasxox"],
+    ["nudel28"],
+    ["SGTCross96"],
+    ["Icefruit"],
+    ["l-IlIllIIlIIllI"]
+];
 
 describe('Squad member relevance check', () => {
     test('relevant', () => {
@@ -19,16 +30,25 @@ describe('find avatar', () => {
         expect(getSquadAvatar("⋇l-IlIllIIlIIllI")).toBe("cardicon_fem_ru_modern_01");
     });
 
-    const KNOWN_SQUAD_MEMBERS = [
-        ["TuxCode"],
-        ["Lukasxox"],
-        ["nudel28"],
-        ["SGTCross96"],
-        ["Icefruit"],
-        ["l-IlIllIIlIIllI"]
-    ];
 
     test.each(KNOWN_SQUAD_MEMBERS)('Squad member check (%s)', (member) => {
         expect(getSquadAvatar(member)).toBeDefined();
+    });
+});
+
+// check if avatar is downloaded if defined
+describe('Team avatar available', () => {
+    test.each(KNOWN_SQUAD_MEMBERS)('Squad member avatar exists (%s)', async (member) => {
+        const file = getSquadAvatar(member);
+        if (!file) {
+            // not found
+            expect(false).toBeTruthy();
+            return;
+        }
+
+        const path = `./src/${AVATAR_FILE_PATH}/${file}.${FILE_EXT}`
+        const exists = await fs.promises.stat(path);
+
+        expect(exists.isFile()).toBeTruthy();
     });
 });
