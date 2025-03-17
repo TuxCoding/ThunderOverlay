4e4a07d471ea25650dd2e26043598d1b9094a8c3
TuxCoding <1957196+TuxCoding@users.noreply.github.com>
2025-03-17 18:23:35 +0100

    Log file errors only if squad relevant or was ignored in general

--- src/network.ts
+++ src/network.ts
@@ -1,26 +1,39 @@
+/** War thunder API host */
 const HOST = "http://localhost:8111"
 
+/**
+ * battle log events
+ */
 export interface HudEvents {
+    /** unknown always empty */
     events: [],
+    /** battle log */
     damage: Damage[]
 };
 
 export interface Damage {
-    // increasing id
+    /** increasing id */
     id: number,
-    // battle log message
+    /** battle log message */
     msg: string,
-    // always empty atm
+    /** always empty */
     sender: string,
-    // always false atm
+    /** always false */
     enemy: boolean,
-    // represents squad, team, all, etc, but is always empty atm
+    /** represents squad, team, all, etc, but is always empty atm */
     mode: string,
-    // battle time in seconds
+    /** battle log timer in seconds */
     time: number
 };
 
+/**
+ * Query new json response
+ *
+ * @param url URL location behind host name
+ * @returns json or error
+ */
 async function query(url: string): Promise<Response> {
+    // web server seems to have no support for other cache control like if-not-modified
     const response = await fetch(`${HOST}/${url}`, {
         method: 'GET',
         headers: {
@@ -35,8 +48,18 @@ async function query(url: string): Promise<Response> {
     return response;
 }
 
+/**
+ * Fetch new hud event message
+ *
+ * @param seenEvent last event id
+ * @param seenDamange last damage id
+ * @returns json response or error
+ */
 export async function fetchHUD(seenEvent: number, seenDamange: number): Promise<HudEvents> {
+    // connect
     const resp = await query(`hudmsg?lastEvt=${seenEvent}&lastDmg=${seenDamange}`);
+
+    // fetch data
     const events = await resp.json();
     return events as HudEvents;
 }
