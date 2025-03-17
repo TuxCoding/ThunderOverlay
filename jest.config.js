487cf3c6a33bf3fae9e448419078a479119f94f1
TuxCoding <1957196+TuxCoding@users.noreply.github.com>
2025-03-17 18:23:35 +0100

    Update deps

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,9 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 module.exports = {
+    // might switch later to happy-dom for browser test environments
     testEnvironment: "node",
+
+    // including only test files
     testMatch: ["**/tests/**/*.test.ts"],
     transform: {
         "^.+.tsx?$": ["ts-jest", {}],
